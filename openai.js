e18e1a2e5dc093bca82890f6f413727db01c91ca
jolyneelhatmi <100769632+jolyneelhatmi@users.noreply.github.com>
2023-07-19 18:55:44 +0100

    Update openai.js LATEST

--- openai.js
+++ openai.js
@@ -11,7 +11,7 @@ const MAX_TOKENS = 500;
 const FREQUENCY_PENALTY = 0;
 const PRESENCE_PENALTY = 0.6;
 const MAX_CONTEXT_QUESTIONS = 10;
-const buttonRoutes = new Map();
+const usersRoutes = {}; // Dictionary to store routes separately for each user
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -24,73 +24,80 @@ app.post('/AddAPI', (req, res) => {
 });
 
 async function getResponse(instructions, previousQuestionsAndAnswers, newQuestion) {
-    const openai = new OpenAIApi(new Configuration({
-      apiKey: apiKey
-    }));
-    const messages = [
-      { role: 'system', content: instructions },
-    ];
-  
-    for (const [question, answer] of previousQuestionsAndAnswers.slice(-MAX_CONTEXT_QUESTIONS)) {
-      messages.push({ role: 'user', content: question });
-      messages.push({ role: 'assistant', content: answer });
-    }
-  
-    messages.push({ role: 'user', content: newQuestion });
-  
-    try {
-      const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        temperature: TEMPERATURE,
-        max_tokens: MAX_TOKENS,
-        top_p: 1,
-        frequency_penalty: FREQUENCY_PENALTY,
-        presence_penalty: PRESENCE_PENALTY
-      });
-    
-  
-      if (completion.data && completion.data.choices && completion.data.choices.length > 0) {
-        return completion.data.choices[0].message.content;
-      } else {
-        throw new Error('Invalid completion response');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-     
-    }
+  const openai = new OpenAIApi(new Configuration({
+    apiKey: apiKey
+  }));
+  const messages = [
+    { role: 'system', content: instructions },
+  ];
+
+  for (const [question, answer] of previousQuestionsAndAnswers.slice(-MAX_CONTEXT_QUESTIONS)) {
+    messages.push({ role: 'user', content: question });
+    messages.push({ role: 'assistant', content: answer });
   }
 
+  messages.push({ role: 'user', content: newQuestion });
 
-  app.post('/create-route', (req, res) => {
-    const { route } = req.body;
-    
-    const routePath = `/${route}`;
-  
-    if (buttonRoutes.has(route)) {
-      buttonRoutes.delete(route);
-    }
-  
-    buttonRoutes.set(route, routePath);
-    buttonRoutes.set(route, []);
-  
-    res.json({ message: 'Route Created' }); 
-  });
-  
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      temperature: TEMPERATURE,
+      max_tokens: MAX_TOKENS,
+      top_p: 1,
+      frequency_penalty: FREQUENCY_PENALTY,
+      presence_penalty: PRESENCE_PENALTY
+    });
 
-
-  app.post('/:route', async (req, res) => {
-    const { route } = req.params;
-    if (buttonRoutes.has(route)) {
-      const input = req.body.input;
-      const previousQuestionsAndAnswers = buttonRoutes.get(route);
-      const response = await getResponse(INSTRUCTIONS, previousQuestionsAndAnswers, input.trim());
-      previousQuestionsAndAnswers.push([input.trim(), response]);
-      res.json({ answer: response }); 
+    if (completion.data && completion.data.choices && completion.data.choices.length > 0) {
+      return completion.data.choices[0].message.content;
     } else {
-      res.sendStatus(404);
+      throw new Error('Invalid completion response');
     }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+app.post('/create-route', (req, res) => {
+  const { userId, route } = req.body;
+  const userRoutes = usersRoutes[userId] || new Map();
+
+  const routePath = `/${route}`;
+
+  if (userRoutes.has(route)) {
+    userRoutes.delete(route);
+  }
+
+  userRoutes.set(route, routePath);
+  userRoutes.set(route, []);
+
+  // Save the userRoutes map in the dictionary for this user
+  usersRoutes[userId] = userRoutes;
+
+  res.json({ message: 'Route Created' });
 });
+
+app.post('/:userId/:route', async (req, res) => {
+  const { userId, route } = req.params;
+  const userRoutes = usersRoutes[userId];
+
+  if (!userRoutes) {
+    res.sendStatus(404);
+    return;
+  }
+
+  if (userRoutes.has(route)) {
+    const input = req.body.input;
+    const previousQuestionsAndAnswers = userRoutes.get(route);
+    const response = await getResponse(INSTRUCTIONS, previousQuestionsAndAnswers, input.trim());
+    previousQuestionsAndAnswers.push([input.trim(), response]);
+    res.json({ answer: response });
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(PORT, () => {
